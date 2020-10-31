2194efdb4f913ffaf7e06450b611c17d49e9782a
FreakyCoder <kaloyankd.06@gmail.com>
2020-10-31 13:36:14 +0200

    Parse (#2)
    * Changed from macmillan to cambridge online dictionary.
    
    * Added function pasers::cambridge to parse response from Cambridge dictionary.
    
    * Added parsers::wordhippo function to parse wordhippo responses.
    
    * Added colorful output.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,39 @@
+mod parsers;
 use reqwest::get;
 use std::env::args;
 use tokio::join;
+use termcolor::{StandardStream, ColorChoice};
 
 async fn fetch(url: &str) -> Result<String, reqwest::Error> {
 	Ok(get(url).await?.text().await?)
 }
 
-async fn fetch_all(word: &str) -> (String, String, String, String, String, String) {
-	let urls: [&str; 6] = [
-		&format!("https://www.macmillandictionary.com/dictionary/british/{}", word)[..],
-		&format!("https://www.macmillandictionary.com/dictionary/american/{}", word)[..],
-		&format!("https://www.wordhippo.com/what-is/the-noun-for/{}.html", word)[..],
+async fn fetch_all(word: &str) -> (String, String, String, String, String) {
+	let urls: [&str; 5] = [
+		&format!("https://dictionary.cambridge.org/dictionary/english/{}", word)[..],
 		&format!("https://www.wordhippo.com/what-is/the-verb-for/{}.html", word)[..],
+		&format!("https://www.wordhippo.com/what-is/the-noun-for/{}.html", word)[..],
 		&format!("https://www.wordhippo.com/what-is/the-adjective-for/{}.html", word)[..],
 		&format!("https://www.wordhippo.com/what-is/the-adverb-for/{}.html", word)[..],
 	];
-	let (r1, r2, r3, r4, r5, r6) = join!(
+	let (r1, r2, r3, r4, r5) = join!(
 		fetch(urls[0]),
 		fetch(urls[1]),
 		fetch(urls[2]),
 		fetch(urls[3]),
 		fetch(urls[4]),
-		fetch(urls[5])
 	);
-	(r1.unwrap(), r2.unwrap(), r3.unwrap(), r4.unwrap(), r5.unwrap(), r6.unwrap())
+	(r1.unwrap(), r2.unwrap(), r3.unwrap(), r4.unwrap(), r5.unwrap()) 
 }
 
 #[tokio::main]
 async fn main() {
+	let mut cout = StandardStream::stdout(ColorChoice::Always);
 	let word = args().nth(1).expect("No word given!");
-	let (t1, t2, t3, t4, t5, t6) = fetch_all(&word).await;	
-	println!("{} {} {} {} {} {}", t1, t2, t3, t4, t5, t6)
+	let (t1, t2, t3, t4, t5) = fetch_all(&word).await;	
+	parsers::cambridge(&t1, &mut cout);
+	parsers::wordhippo(&t2, &mut cout, "(v.)");
+	parsers::wordhippo(&t3, &mut cout, "(n.)");
+	parsers::wordhippo(&t4, &mut cout, "(adj.)");
+	parsers::wordhippo(&t5, &mut cout, "(adv.)");
 }
---
+++ src/parsers.rs
@@ -0,0 +1,34 @@
+use select::document::Document;
+use select::predicate::Class;
+use termcolor::{Color, ColorSpec, StandardStream, WriteColor};
+use std::io::Write;
+
+pub fn cambridge(document: &str, cout: &mut StandardStream) {
+	cout.reset().unwrap();
+	let document = Document::from(document);
+	for (i, block) in document.select(Class("def-block")).enumerate() {
+		let block_document = Document::from(&block.html()[..]);
+		for definition in block_document.select(Class("def")) {
+			cout.set_color(ColorSpec::new().set_fg(Some(Color::Green))).unwrap();
+			writeln!(cout, "{}. {}", i + 1, definition.text()).unwrap();
+			cout.set_color(ColorSpec::new().set_fg(Some(Color::Blue))).unwrap();
+			writeln!(cout, "Examples: ").unwrap();
+			for (i, example) in block_document.select(Class("examp")).enumerate() {
+				let example_text = match i {
+					0 => example.text(),
+					_ => example.text()[1..].to_string(),
+				};
+				writeln!(cout, "	{}", example_text).unwrap();
+			}
+		}
+	}
+}
+
+pub fn wordhippo(document: &str, cout: &mut StandardStream, form_type: &str) {
+	cout.set_color(ColorSpec::new().set_fg(Some(Color::Red))).unwrap();
+	let document = Document::from(document);
+	for word in document.select(Class("defv2wordtype")) {
+		write!(cout, "{}{} ", word.text(), form_type).unwrap();
+	}
+	writeln!(cout, "").unwrap();
+}
